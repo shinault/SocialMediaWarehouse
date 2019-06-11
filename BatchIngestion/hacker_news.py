71566eb5ae1d8eaae06fe4f2bbc82ddb3cb47d59
Gregory M. Shinault <gshinault@gmail.com>
2019-06-11 15:02:33 -0700

    Add statements on progress for Hacker News collection

--- BatchIngestion/hacker_news.py
+++ BatchIngestion/hacker_news.py
@@ -7,6 +7,9 @@ json_filename = "14m_hn_comments_sorted.json"
 s3_location = "raw/hacker_news/" + json_filename
 
 if __name__ == "__main__":
+    print("Downloading file")
     DataCollector.download(hn_bzipped_comments_url, bzipped_filename)
+    print("Decompressing file")
     DataCollector.decompress(bzipped_filename, json_filename, "bz2")
+    print("Transferring file to S3")
     DataCollector.write_to_S3(json_filename, s3_location, "saywhat-warehouse")
