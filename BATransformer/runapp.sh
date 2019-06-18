8e4977be1a7cc345e196ce652986f5c76b8a09cc
Gregory M. Shinault <gshinault@gmail.com>
2019-06-18 14:04:53 -0700

    Add Databrick spark-xml jar to submit command for transformer

--- BATransformer/runapp.sh
+++ BATransformer/runapp.sh
@@ -2,9 +2,8 @@
 
 spark-submit \
     --class App \
-    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar: \
-    --jars /usr/share/java/postgresql.jar,/usr/local/spark/lib/hadoop-aws-2.7.1.jar,/usr/local/spark/lib/aws-java-sdk-1.7.4.jar \
-    --packages "com.databricks:spark-xml_2.11:0.5.0"
+    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar:/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar \
+    --jars /usr/share/java/postgresql.jar,/usr/local/spark/lib/hadoop-aws-2.7.1.jar,/usr/local/spark/lib/aws-java-sdk-1.7.4.jar,/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar \
     target/scala-2.11/business-analysis-transformer_2.11-1.0.jar \
     $1 \
     $2 
