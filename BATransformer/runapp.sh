1685bcfedbec628ee52346247dfe9600882df498
Gregory M. Shinault <gshinault@gmail.com>
2019-06-18 12:44:11 -0700

    Fix syntax in runapp.sh for jars and driver-class-path

--- BATransformer/runapp.sh
+++ BATransformer/runapp.sh
@@ -2,8 +2,8 @@
 
 spark-submit \
     --class App \
-    --driver-class-path /usr/share/java/postgresql.jar \
-    --jars /usr/share/java/postgresql.jar /usr/local/spark/lib/hadoop-aws-2.7.1.jar \
+    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar \
+    --jars /usr/share/java/postgresql.jar,/usr/local/spark/lib/hadoop-aws-2.7.1.jar \
     target/scala-2.11/business-analysis-transformer_2.11-1.0.jar \
     $1 \
     $2 
