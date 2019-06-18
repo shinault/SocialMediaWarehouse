a1bbf747cfe2a94f18fff408761f784a77df0a53
Gregory M. Shinault <gshinault@gmail.com>
2019-06-18 13:58:39 -0700

    Add XML package to spark-submit script for transformer

--- BATransformer/runapp.sh
+++ BATransformer/runapp.sh
@@ -2,8 +2,9 @@
 
 spark-submit \
     --class App \
-    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar \
+    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar: \
     --jars /usr/share/java/postgresql.jar,/usr/local/spark/lib/hadoop-aws-2.7.1.jar,/usr/local/spark/lib/aws-java-sdk-1.7.4.jar \
+    --packages "com.databricks:spark-xml_2.11:0.5.0"
     target/scala-2.11/business-analysis-transformer_2.11-1.0.jar \
     $1 \
     $2 
