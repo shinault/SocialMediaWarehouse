46fc29cfeae0e0b75cd6fc9a00a633ae37a38246
Gregory M. Shinault <gshinault@gmail.com>
2019-06-20 22:21:45 -0700

    Update run script

--- BATransformer/runapp.sh
+++ BATransformer/runapp.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+DCPATH=/usr/share/java/postgresql.jar
+DCPATH=$DCPATH:/usr/local/spark/lib/hadoop-aws-2.7.1.jar
+DCPATH=$DCPATH:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar
+DCPATH=$DCPATH:/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar
+
+JARS=/usr/share/java/postgresql.jar
+JARS=$JARS,/usr/local/spark/lib/hadoop-aws-2.7.1.jar
+JARS=$JARS,/usr/local/spark/lib/aws-java-sdk-1.7.4.jar
+JARS=$JARS,/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar
+
 spark-submit \
     --class App \
-    --driver-class-path /usr/share/java/postgresql.jar:/usr/local/spark/lib/hadoop-aws-2.7.1.jar:/usr/local/spark/lib/aws-java-sdk-1.7.4.jar:/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar \
-    --jars /usr/share/java/postgresql.jar,/usr/local/spark/lib/hadoop-aws-2.7.1.jar,/usr/local/spark/lib/aws-java-sdk-1.7.4.jar,/usr/local/spark/lib/spark-xml_2.11-0.5.0.jar \
+    --driver-class-path $DCPATH \
+    --jars $JARS \
+    --master $SPARK_MASTER_PATH \
     target/scala-2.11/business-analysis-transformer_2.11-1.0.jar \
     $1 
