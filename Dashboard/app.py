25f08ba921f085561db4a41119df9512831c6786
Gregory M. Shinault <gshinault@gmail.com>
2019-07-02 22:25:43 -0700

    Add visualization for sample app

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, redirect, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_table import Col, Table
 from flask_wtf import FlaskForm
+import matplotlib.pyplot as plt
+import pandas as pd
 from wtforms import SelectField, StringField, SubmitField
 from wtforms.fields.html5 import DateField, DateTimeField
 from wtforms.validators import DataRequired
 import xml.etree.ElementTree as ET
 
+import base64
+import io
 import os
 import psycopg2
 
@@ -47,6 +51,34 @@ def all_se_sources():
             output.append((source, source))
     return output
 
+def fill_feb(df):
+    for day in range(30):
+        date = '2012-02-{}'.format(day)
+        if date not in df.Day:
+            df.append({"Day": date, "Count": 0}, ignore_index=True)
+
+
+# Technique from blog post technovechno.com
+def make_plot(query_data):
+    img = io.BytesIO()
+    ## Create plot instructions
+    df = pd.DataFrame(query_data, columns=["Day", "Source", "Count"])
+    reddit = df.loc[df.Source=='reddit', ['Day','Count']]
+    se = df.loc[df.Source=='stackexchange', ['Day','Count']]
+    hn = df.loc[df.Source=='hackernews', ['Day','Count']]
+    fill_feb(reddit)
+    fill_feb(se)
+    fill_feb(hn)    
+    plt.plot('Day', 'Count', data=reddit, marker='', color='skyblue', linewidth=4, label="Reddit")
+    plt.plot('Day', 'Count', data=se, marker='', color='olive', linewidth=4, label="Stack Exchange")
+    plt.plot('Day', 'Count', data=hn, marker='', color='orange', linewidth=4, label="Hacker News")
+    plt.legend()
+    
+    plt.savefig(img, format='png')
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return 'data:image/png;base64,{}'.format(graph_url)
 
 ## Config
 app = Flask(__name__)
@@ -68,12 +100,6 @@ app.config["SECRET_KEY"] = os.environ["FLASK_SECRET"]
 
 ## Forms
 class AppForm(FlaskForm):
-    fromDate = DateField("Initial date:",
-                       format='%Y-%m-%d',
-                       validators=[DataRequired()])
-    toDate = DateField("Terminal date:",
-                       format='%Y-%m-%d',
-                       validators=[DataRequired()])
     searchString = StringField("Search comments for this term:")
     submit = SubmitField("Submit")
 
@@ -91,7 +117,6 @@ class RedditForm(FlaskForm):
 class SEForm(FlaskForm):
     table = SelectField("Raw Data", choices = all_se_sources())
     submit = SubmitField("Submit")
-
 
 
 ## Routes
@@ -140,7 +165,7 @@ def stackexchange():
 
 @app.route("/catalog/hackernews", methods=["GET"])
 def hackernews():
-    conn = psycopg2.connect("dbname='comments' user='{}' host='{}' password='{}' port='{}'"
+    conn = psycopg2.connect("dbname='dictionaries' user='{}' host='{}' password='{}' port='{}'"
                             .format(dict_user, dict_host, dict_pw, dict_port))
     cur = conn.cursor()
     table_name = "hackernews"
@@ -154,35 +179,23 @@ def hackernews():
 
 @app.route("/sample", methods=["GET", "POST"])
 def sample():
-    table = None
+    graph = None
     form = AppForm()
     if form.validate_on_submit():
-        conn = psycopg2.connect("dbname='dictionaries' user='{}' host='{}' password='{}' port='{}'"
+        conn = psycopg2.connect("dbname='comments' user='{}' host='{}' password='{}' port='{}'"
                                 .format(dict_user, dict_host, dict_pw, dict_port))
         cur = conn.cursor()
-        fromDate = form.fromDate.data
-        toDate = form.toDate.data
-        search = form.searchString.data
-        select_statement = """SELECT source, COUNT(*) as count from long_comments """
-        where_clause = """WHERE datetime>='{}' AND datetime<='{}' AND text LIKE '%{}%'
-GROUP BY source"""
-        cur.execute((select_statement + where_clause).format(fromDate, toDate, search))
+        search_term = form.searchString.data
+        select_statement = """SELECT source, DATE(datetime) AS day COUNT(*) AS count FROM long_comments """
+        where_clause = """text LIKE '%{}%' GROUP BY source, day"""
+        cur.execute((select_statement + where_clause).format(search_term))
         count_rows = cur.fetchall()
-        count_dicts = map(lambda x: dict(source=x[0], count=x[1], count_rows))
-        table = CommentTable(count_dicts)
-    return render_template("sample.html", form=form, table=table)
-
-
-## Tables
-class CommentTable(Table):
-    source = Col('Source')
-    count = Col('Total')
-
-class DictTable(Table):
-    variable = Col("Variable")
-    type = Col("Type")
-    description = Col("Description")
+        cur.close()
+        conn.close()
+        graph = make_plot(count_rows)
+    return render_template("sample.html", form=form, graph=graph)
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0", port=80)
